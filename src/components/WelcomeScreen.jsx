d1e1a004f4a612bea92d234245ff180b12db37eb
hadar <hadarbublil2@gmail.com>
2024-10-31 11:27:01 +0200

    design welcomeScreen

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -2,9 +2,9 @@ import question_mark from '../images/question_mark.jpg'
 import "./WelcomeScreen.css";
 const WelcomeScreen = ({ onStart }) => (
     <>
-      <h1 id="headline">MyTrivia</h1>
+      <h1 id="headline">Welcome to MyTrivia</h1>
       <img className="question-mark" src={question_mark} alt="Question Mark" />
-      <h3>Are you ready to start?</h3>
+      <h3 className = "start"> Are you ready to start?</h3>
       <button className="btn" id="yes" onClick={onStart}>
         Yes
       </button>
